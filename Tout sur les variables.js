5a67b67d0a5b75bc91482d5bef1bfc4ba27d171d
Samuel2698 <97331279+Samuel2698@users.noreply.github.com>
2022-03-09 18:20:19 +0100

    Add files via upload

---
+++ Tout sur les variables.js
@@ -0,0 +1,31 @@
+string = "cadena de texto"
+number = 19 /* "19" est un string */
+booleano =  false
+
+const nombre = "Samuel" /* la variable const ne peut changer de valeur */
+
+/* let numero;
+numero = 29 */
+
+let numero = 29; /*On doit ajouter à la fin le " ; " */
+
+alert(numero);
+
+
+let numero1, numero2, numero3;
+
+numero1 = 1;
+numero2 = 2;
+numero3 = 3;
+
+alert(numero1);
+alert(numero2);
+alert(numero3);
+
+/** let numero1 = 23, numero2 = 39; 
+ * ! ou on peut aussi faire:
+ * let numero1 = 23;
+ * let numero2 = 39;
+ * *pour que ça soit mieux ordoné on le fait dans deux lignes
+*/
+
