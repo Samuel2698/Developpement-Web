401117d553f269c6a1615380aea22a4c1f46da91
Samuel2698 <97331279+Samuel2698@users.noreply.github.com>
2022-03-10 09:42:12 +0100

    Add files via upload

---
+++ Demander des données avec prompt.js
@@ -0,0 +1,8 @@
+let prenom = prompt("dit-moi ton prenom");
+alert("Salut " + prenom);
+
+let nombre = prompt("dime tu nombre");
+alert("Hola " + nombre);
+
+let firstname = prompt("tell me your first name");
+alert("Hi " + firstname);
