2e26937c2126c91c00731b8884b1475c6d6bfd02
Samuel2698 <97331279+Samuel2698@users.noreply.github.com>
2022-03-13 15:13:14 +0100

    Add files via upload

---
+++ Conditionnels.js
@@ -0,0 +1,51 @@
+if (true) { //Si true le block {} va s'exécuter
+    alert("Salut");
+}
+
+if (10 > 2) { //s'exécute
+    alert("Bonjour");
+}
+
+if (10 > 22) { //s'exécute pas
+    alert("Bonsoir");
+}
+
+if ("Pedro" == "Pedro") { //s'exécute
+    alert("Bonsoir");
+}
+
+let nombre = "Samuel Velasco";
+
+if (nombre == "Samuel Velasco") { //s'exécute
+    alert("Hey");
+}
+
+let username = "Lee";
+
+if (username == "Ben"){
+    alert("XD");
+}
+else if (username == "Lee"){ //s'exécute
+    alert("XD");
+}
+
+let prenom = "Hugo";
+
+if (prenom == "Pierre"){
+    alert("ça va?");
+}
+
+else if (prenom == "Max"){
+    alert("ça va?");
+}
+
+else{
+    alert("...ça va?");
+}
+
+let minombre = "Samuel";
+let miapellido = "Velasco";
+
+if (minombre == "Samuel" && miapellido == "Velasco"){
+    alert(":D")
+}
