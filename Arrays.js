62197c22aa8219ab5d9a6ee7871c230d3bfa73c5
Samuel2698 <97331279+Samuel2698@users.noreply.github.com>
2022-03-14 09:19:02 +0100

    Add files via upload

---
+++ Arrays.js
@@ -0,0 +1,9 @@
+frutas = ["banana", " manzana", " pera"];
+//commence à 0, donc "banana"
+
+document.write(frutas[1]); //ça va montrer manzana
+document.write(frutas[3]); //ça montre undefined
+
+let nombres = [3,77,43,886,24,66666,7,443,1];
+document.write(nombres[6]); //ça montre 7
+
