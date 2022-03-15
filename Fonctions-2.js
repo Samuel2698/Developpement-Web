d2aeb6ac43d3f68048e7b4b9209869c9b8837f14
Samuel2698 <97331279+Samuel2698@users.noreply.github.com>
2022-03-15 11:35:52 +0100

    Add files via upload

---
+++ Fonctions-2.js
@@ -0,0 +1,12 @@
+
+function saludar() {
+    
+    alert("hola");
+    return 3; //le return termine la fonction
+              //le retrun doit donc être après alert
+
+}
+
+let saludo = saludar();
+
+document.write(saludo);
