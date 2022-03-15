ee33b6893b83085f8577b28d3293922d232d87f3
Samuel2698 <97331279+Samuel2698@users.noreply.github.com>
2022-03-15 09:58:43 +0100

    Add files via upload

---
+++ Fonctions-1.js
@@ -0,0 +1,17 @@
+
+function saludar(){
+
+    respuesta = prompt("Hola Samuel! Como fue tu dia?");
+
+if (respuesta == "bien") {
+    alert ("me alegro")
+
+}   else {
+    alert("una pena")
+}
+}
+
+saludar()
+saludar()
+saludar()
+saludar() //donc on va répéter le code 4 fois
