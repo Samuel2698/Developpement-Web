a082bbe23d2dd63e9a73d5adebfc8064f5031ca5
Samuel2698 <97331279+Samuel2698@users.noreply.github.com>
2022-03-20 09:03:46 +0100

    Add files via upload

---
+++ bmi.hs
@@ -0,0 +1,34 @@
+--Fonction qui vous classe en fonction de votre IMC (BMI en anglais):
+
+bmiTell :: (RealFloat a) => a -> String
+bmiTell bmi
+    | bmi <= 18.5 = "You're underweight, you emo, you!"
+    | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
+    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"
+    | otherwise   = "You're a whale, congratulations!"
+
+--Cette fois on doit apporter notre taille et masse pour que la fonction nous calcule notre IMC
+
+bmiTell' :: (RealFloat a) => a -> a -> String
+bmiTell' weight height
+    | weight / height ^ 2 <= 18.5 = "You're underweight, you emo, you!"
+    | weight / height ^ 2 <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
+    | weight / height ^ 2 <= 30.0 = "You're fat! Lose some weight, fatty!"
+    | otherwise                 = "You're a whale, congratulations!"
+
+--Cela facilite la lisibilité en donnant des noms aux choses
+
+bmiTell'' :: (RealFloat a) => a -> a -> String
+bmiTell'' weight height
+    | bmi <= skinny = "You're underweight, you emo, you!"
+    | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
+    | bmi <= fat    = "You're fat! Lose some weight, fatty!"
+    | otherwise     = "You're a whale, congratulations!"
+    where bmi = weight / height ^ 2
+          skinny = 18.5
+          normal = 25.0
+          fat = 30.0
+
+
+
+
