610845b630c3791a10ef62567b3b906203038308
Samuel2698 <97331279+Samuel2698@users.noreply.github.com>
2022-03-25 09:09:51 +0100

    Update and rename Attributs d'un élément.js to Attributs-1.js

--- Attributs-1.js
+++ Attributs-1.js
@@ -19,3 +19,18 @@ valorDelAtributo = rangoEtario.setAttribute("type", "");
 valorDelAtributo = rangoEtario.removeAttribute("type");
 //On a plus de type
 
+const titulo = document.querySelector(".titulo");
+
+titulo.setAttribute("contentEditable", "true");
+//on peut éditer .titulo de h1
+
+titulo.setAttribute("contentEditable", "false");
+//maintenant on peut pas modifier le h1
+
+titulo.removeAttribute("hidden");
+/* 
+titulo.setAttribute("tabindex", "0"); */
+
+titulo.setAttribute("title", "jajaja xd");
+// ça affecte title="titulo-normal dans index.html
+
