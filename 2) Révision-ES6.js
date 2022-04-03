47edd86f550e1bf217d594a8212c02ce727bb3fe
Samuel2698 <97331279+Samuel2698@users.noreply.github.com>
2022-04-03 14:31:19 +0200

    Add files via upload

--- 2) Révision-ES6.js
+++ 2) Révision-ES6.js
@@ -289,3 +289,174 @@ function makeList(arr) {
 }
 
 const failureList = makeList(result.failure); 
+
+// ! PARTIE 2
+
+
+//* Write Concise Object Literal Declarations Using Object Property Shorthand
+
+// Au lieu de:
+const createPerson = (name, age, gender) => {
+  return {
+    name: name,
+    age: age,
+    gender: gender
+  };
+};
+
+// On peut faire à la place:
+const createPerson1 = (name, age, gender) => {
+  return {
+    name,
+    age,
+    gender
+  };
+};
+
+//* Write Concise Declarative Functions with ES6
+
+const bicycle = {
+  gear: 2,
+  setGear(newGear) { // Au lieu de setGear function(newGear) 
+    this.gear = newGear;
+  }
+};
+bicycle.setGear(3);
+console.log(bicycle.gear);
+
+//* Use class Syntax to Define a Constructor Function
+
+class Vegetable {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+const carrot = new Vegetable("carrot");
+console.log(carrot.name); // carrot 
+
+//* Use getters and setters to Control Access to an Object
+
+class Thermostat {
+  constructor(fahrenheit) {
+    this.fahrenheit = fahrenheit;
+  }
+  
+  get temperature() {
+    return (5 / 9) * (this.fahrenheit - 32);
+  }
+  
+  set temperature(celsius) {
+    this.fahrenheit = (celsius * 9.0) / 5 + 32;
+  }
+}
+
+const thermos = new Thermostat(76); 
+let temp = thermos.temperature;
+thermos.temperature = 26;
+temp = thermos.temperature; 
+
+//* Create a Module Script
+
+{/* <body>
+<script type="module" src="index.js"></script>
+</body> */}
+
+//* Use export to Share a Code Block
+
+const uppercaseString = (string) => {
+  return string.toUpperCase();
+}
+
+const lowercaseString = (string) => {
+  return string.toLowerCase()
+}
+
+/* export {uppercaseString};
+export {lowercaseString}; */
+
+// On peut aussi faire: 
+// ! export {uppercaseString, lowercaseString};
+
+//* Reuse JavaScript Code Using import
+
+//import {uppercaseString, lowercaseString} from './string_functions.js';
+// ! attention au ./ après from ''
+uppercaseString("hello");
+lowercaseString("WORLD!");
+
+//* Use * to Import Everything from a File
+
+//import * as stringFunctions from './string_functions.js';
+/* 
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!"); */
+
+//* Create an Export Fallback with export default
+/* 
+export default function subtract(x, y) {
+  return x - y;
+}
+ */
+//* Import a Default Export
+
+//import subtract from "./math_functions.js";
+// ! pas besoin des {} dans ce cas
+//subtract(7,4);
+
+//* Create a JavaScript Promise
+
+const makeServerRequest1 = new Promise((resolve, reject) => {
+
+});
+
+//* Complete a Promise with resolve and reject
+/* 
+const makeServerRequest = new Promise((resolve, reject) => {
+  let responseFromServer;
+  
+if(responseFromServer) {
+  resolve("We got the data");
+} else {  
+  reject("Data not received");
+}
+}); */
+
+//* Handle a Fulfilled Promise with then
+
+/* const makeServerRequest = new Promise((resolve, reject) => {
+  let responseFromServer = true;
+    
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {  
+    reject("Data not received");
+  }
+});
+
+makeServerRequest.then(result => {
+  console.log(result);
+}); */
+
+//* Handle a Rejected Promise with catch
+
+const makeServerRequest = new Promise((resolve, reject) => {
+  let responseFromServer = false;
+    
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {  
+    reject("Data not received");
+  }
+});
+
+makeServerRequest.then(result => {
+  console.log(result);
+});
+
+makeServerRequest.catch(error => {
+  console.log(error)
+});
+
+// ? The end :)
+
